a29d458c6d3137746aa411cc72d8f1e581cccd65
masterUNG <phrombutr@gmail.com>
2015-12-05 14:55:01 +0700

    Intent To WebView Finish

--- app/src/main/java/appewtc/masterung/mytraffic/MainActivity.java
+++ app/src/main/java/appewtc/masterung/mytraffic/MainActivity.java
@@ -1,6 +1,8 @@
 package appewtc.masterung.mytraffic;
 
+import android.content.Intent;
 import android.media.MediaPlayer;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -36,6 +38,12 @@ public class MainActivity extends AppCompatActivity {
                 //Sound Effect
                 MediaPlayer buttonMediaPlayer = MediaPlayer.create(getBaseContext(), R.raw.cow);
                 buttonMediaPlayer.start();
+
+                //Intent to WebView
+                Intent objIntent = new Intent(Intent.ACTION_VIEW);
+                objIntent.setData(Uri.parse("https://youtu.be/AFmWqLIqDZA"));
+                startActivity(objIntent);
+
 
             }   // event
         });
