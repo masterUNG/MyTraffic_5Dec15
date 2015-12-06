953de974b381953e1c510d2c4b8ff70c34ac64b2
masterUNG <phrombutr@gmail.com>
2015-12-06 14:49:18 +0700

    Sound Intro Finish

--- app/src/main/java/appewtc/masterung/mytraffic/SplashScreen.java
+++ app/src/main/java/appewtc/masterung/mytraffic/SplashScreen.java
@@ -1,5 +1,8 @@
 package appewtc.masterung.mytraffic;
 
+import android.content.Intent;
+import android.media.MediaPlayer;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -9,5 +12,20 @@ public class SplashScreen extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash_screen);
-    }
-}
+
+        Handler objHandler = new Handler();
+        objHandler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent objIntent = new Intent(SplashScreen.this, MainActivity.class);
+                startActivity(objIntent);
+                finish();
+            }
+        }, 8000);
+
+        MediaPlayer introMediaPlayer = MediaPlayer.create(getBaseContext(), R.raw.intro_tata);
+        introMediaPlayer.start();
+
+    }   // Main Method
+
+}   // Main Class
